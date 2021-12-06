b00738c787da08f196c11d663e94306df9415129
william ashbee <wsashbee@gmail.com>
2021-12-05 19:21:10 -0500

    display method is partially working

--- algorithmsProject.py
+++ algorithmsProject.py
@@ -3,14 +3,11 @@
 #pip install pulp
 from pulp import *
 import numpy as np
-#  
-#      a-b
-#      | |
-#      c-d
-#
+import matplotlib.pyplot as plt
 
+vert_to_cart = {'a':(0,0),'b':(1,0),'c':(1,1),'d':(0,1)}
 verticies = ['a','b','c','d']
-distances = np.array([[0,1,1,1.5],[1,0,1.5,1],[1,1.5,0,1],[1.5,1,1,0]])
+distances = np.array([[0,1,np.sqrt(2),1],[1,0,1,np.sqrt(2)],[np.sqrt(2),1,0,1],[1,np.sqrt(2),1,0]])
 problem = LpProblem('Car Factory', LpMinimize)
 
 
@@ -18,26 +15,19 @@ edges = []
 
 inci_dict = {i:[] for i in verticies}
 
+edges_dict = dict()
+cartesian_dict = dict()
+
 for vi in range(len(verticies)):
     for vj in range(vi+1,len(verticies)):
         s = verticies[vi]+str('_')+verticies[vj]
         var = (LpVariable(s, cat="Binary"),distances[vi,vj])
         inci_dict[verticies[vi]].append(var)
         inci_dict[verticies[vj]].append(var)
-        
-        
         edges.append(var)
-
+        cartesian_dict[s]=(vert_to_cart[verticies[vi]][0],vert_to_cart[verticies[vi]][1],vert_to_cart[verticies[vj]][0],vert_to_cart[verticies[vj]][1])
+        edges_dict[s] = var
 assert len(edges)==6
-# # # ab = LpVariable('ab', cat="Binary")
-# # # ac = LpVariable('ac', cat="Binary")
-# # # ad = LpVariable('ad', cat="Binary")
-# # # bc = LpVariable('bc', cat="Binary")
-# # # bd = LpVariable('bd', cat="Binary")
-# # # cd = LpVariable('cd', cat="Binary")
-# # # edges = [(ab,1),(ac,1),(ad,1.5),(bc,1.5),(bd,1),(cd,1)]
-
-#Objective Function
 obj = LpAffineExpression(edges)
 print ('objective',obj)
 problem+= obj
@@ -54,7 +44,7 @@ for vert in verticies:
     ec = LpAffineExpression(inci_dict[vert])
     ec = LpConstraint(e=ec, sense=1, name='incivert_'+vert, rhs=2)
     problem+=ec
-        
+
 
 print("Current Status: ", LpStatus[problem.status])
 
@@ -63,8 +53,22 @@ problem.solve()
 for edge in edges:
     print('edge',edge[1],edge[0].varValue)
 
+
 print("Total edges: ", value(problem.objective))
 
 print(problem.objective)
 for a,b in enumerate(problem.constraints):
     print(a,b)
+
+fig = plt.figure()
+ax = plt.axes(projection='3d')
+
+for ind,key in enumerate(edges_dict):
+    edge = edges_dict[key]
+    print(edge,key)
+    if edge[0].varValue == 1.0:
+        x = [cartesian_dict[key][0],cartesian_dict[key][2]]
+        y = [cartesian_dict[key][1],cartesian_dict[key][3]]
+        plt.plot(x,y,color='green')
+        
+plt.show()
