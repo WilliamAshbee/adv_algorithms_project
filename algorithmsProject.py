ae702c291515008b2661c035adb432d2600a3bca
william ashbee <wsashbee@gmail.com>
2021-12-07 23:59:43 -0500

    working for circle

--- algorithmsProject.py
+++ algorithmsProject.py
@@ -7,8 +7,9 @@ import matplotlib.pyplot as plt
 from scipy.spatial import distance
 
 #verts = np.array([[0,0,0],[0,1,0],[1,0,0],[1,1,0]])
-theta = np.linspace(0,3.0/2.0,4)*3.14159
-verts = np.zeros((4,3))
+points = 3.0
+theta = np.linspace(0,2.0-2.0/points,int(points))*3.14159
+verts = np.zeros((int(points),3))
 verts[:,0] = np.cos(theta)#array([ 1.00000000e+00,  1.32679490e-06, -1.00000000e+00, -3.98038469e-06])
 verts[:,1] = np.sin(theta)#array([ 0.00000000e+00,  1.00000000e+00,  2.65358979e-06, -1.00000000e+00])
 print(verts)
@@ -33,12 +34,16 @@ cartesian_dict = dict()
 for vi in range(len(verticies)):
     for vj in range(vi+1,len(verticies)):
         s = verticies[vi]+str('_')+verticies[vj]
-        var = (LpVariable(s, cat="Binary"),distances[vi,vj])
-        inci_dict[verticies[vi]].append(var)
-        inci_dict[verticies[vj]].append(var)
-        edges.append(var)
+        var = LpVariable(s, cat="Binary")
+        var_edge_weights = (var,distances[vi,vj])
+        var_degree = (var,1)
+        inci_dict[verticies[vi]].append(var_degree)
+        inci_dict[verticies[vj]].append(var_degree)
+        edges.append(var_edge_weights)
         cartesian_dict[s]=(vert_to_cart[verticies[vi]][0],vert_to_cart[verticies[vi]][1],vert_to_cart[verticies[vj]][0],vert_to_cart[verticies[vj]][1])
-        edges_dict[s] = var
+        edges_dict[s] = var_edge_weights
+
+
 assert len(edges)==(verts.shape[0]*(verts.shape[0]-1)//2)
 obj = LpAffineExpression(edges)
 print ('objective',obj)
@@ -53,6 +58,8 @@ assert numedges == nVert*(nVert-1)//2
 
 for vert in verticies:
     assert len(inci_dict[verticies[vi]]) == verts.shape[0]-1
+    print(inci_dict[verticies[vi]])
+    #exit()
     ec = LpAffineExpression(inci_dict[vert])
     ec = LpConstraint(e=ec, sense=1, name='incivert_'+vert, rhs=2)
     problem+=ec
